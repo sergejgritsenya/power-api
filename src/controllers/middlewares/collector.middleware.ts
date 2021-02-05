3c99fc003b1dadf07e64d684776de88f1ae32e22
Sergej <sergejgritsenya@gmail.com>
2021-02-05 13:16:45 +0300

    update prettier

--- src/controllers/middlewares/collector.middleware.ts
+++ src/controllers/middlewares/collector.middleware.ts
@@ -4,7 +4,6 @@ import { NextFunction, Request, Response } from "express"
 export const collectorMiddleware = async (req: Request, _res: Response, next: NextFunction) => {
   try {
     const url = process.env.COLLECTOR_URL
-    console.log(req.ip)
     if (url) {
       await axios.post(url, { ip: req.ip })
     }
