acc49e7aec692c6868cea50ef9441dba0e548977
Sergej <sergejgritsenya@gmail.com>
2021-04-07 19:38:09 +0300

    rm collector

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,13 @@ import { Logger } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import cors from "cors"
 import { config } from "./config"
-import { collectorMiddleware } from "./controllers/middlewares"
 import { AppModule } from "./modules"
 
 const bootstrap = async () => {
   new Logger().log(`App config: ${JSON.stringify(config)}`)
   const app = await NestFactory.create(AppModule)
   app.use(cors())
-  app.use(collectorMiddleware)
-  await app.listen(config.PORT, () =>
-    Logger.log(`Server start on: http://localhost:${config.PORT}`)
-  )
+  // app.use(collectorMiddleware)
+  await app.listen(config.PORT, () => Logger.log(`Server start on: http://localhost:${config.PORT}`))
 }
 bootstrap()
